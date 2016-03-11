60b6a6195a590cc2c29a4f9befeeea2cf6eb5b1d
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-03-11 08:34:21 +0100

    Added IBootregistrar interface

---
+++ Sources/Our.Umbraco.BootEasy/Interfaces/IBootRegistrar.cs
@@ -0,0 +1,23 @@
+﻿namespace Our.Umbraco.BootEasy.Interfaces
+{
+    /// <summary>
+    /// The BootRegistrar interface.
+    /// </summary>
+    internal interface IBootRegistrar
+    {
+        /// <summary>
+        /// Gets the sort order.
+        /// </summary>
+        int SortOrder { get; }
+
+        /// <summary>
+        /// Gets the boot type.
+        /// </summary>
+        Enums.BootType BootType { get; }
+
+        /// <summary>
+        /// The register method
+        /// </summary>
+        void Register();
+    }
+}
